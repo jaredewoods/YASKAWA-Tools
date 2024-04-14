06a0b20687760515a326d10da0701730948805d9
jaredewoods <155901160+jaredewoods@users.noreply.github.com>
2024-04-13 21:48:30 -0700

    TRANSFER

--- alarm_analyzer.py
+++ alarm_analyzer.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import tkinter.font as tkFont
-
+import pyinstaller
 # Embedded alarm dictionary with the specified alarm and sub-codes
 alarm_dict = {
     "0020": {
